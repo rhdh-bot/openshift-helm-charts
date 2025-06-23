42091b34fabc4cd0aa93eb2870b36d36c32605c8
RHDH Build (rhdh-bot) <rhdh-bot@redhat.com>
2025-06-23 14:23:58 +0000

    chore: add backstage-1.7-95-CI
    Signed-off-by: RHDH Build (rhdh-bot) <rhdh-bot@redhat.com>

--- installation/install.sh
+++ installation/install.sh
@@ -20,7 +20,7 @@ usage ()
 
 Examples:
   $0 1.1.1 
-  $0 1.7-94-CI -n rhdh-ci
+  $0 1.7-95-CI -n rhdh-ci
 
 Options:
   -n, --namespace   Project or namespace into which to install specified chart; default: $namespace
